6c854a8a5cea22cb3aa2d49e7a0c72e60da3e315
Sinan B. Karaağaç <sinanberkk94@gmail.com>
2022-12-02 18:02:05 +0300

    test

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,108 +5,101 @@ module.exports = {
     // Your Name
     name: 'Sinan Berk Karaagac',
     // Main Site Title
-        title: `Sinan Berk Karaagac | Gameplay Programmer`,
+    title: `Sinan Berk Karaagac | Gameplay Programmer`,
     // Description that goes under your name in main bio
     description: `Me ;)`,
     // Optional: Twitter account handle
     author: ``,
     // Optional: Github account URL
-     github: `https://github.com/FindikBabmbino`,
+    github: `https://github.com/FindikBabmbino`,
     // Optional: LinkedIn account URL
     linkedin: `https://www.linkedin.com/in/sinan-berk-karaa%C4%9Fa%C3%A7-9a139313a/`,
     // Content of the About Me section
-        fiverr: `https://www.fiverr.com/findikbabi/create-new-gameplay-functions-or-fix-your-code?context_referrer=search_gigs&source=top-bar&ref_ctx_id=78ae1dfffa92aca3e7e92c8021a1e76f&pckg_id=1&pos=3&context_type=auto&funnel=2cb46aa83855a6a52600bbd7dea24cc5&seller_online=true`,
-        about: `I am a hardworking game dev who wants to make games that can bring joy to people, to me games are an art form that can make people escape from their realities and let them forget about their stressful daily life, and inspire them by the stories and characters they play. I am skilled in writing, game design, C++ and C#.`,
+    fiverr: `https://www.fiverr.com/findikbabi/create-new-gameplay-functions-or-fix-your-code?context_referrer=search_gigs&source=top-bar&ref_ctx_id=78ae1dfffa92aca3e7e92c8021a1e76f&pckg_id=1&pos=3&context_type=auto&funnel=2cb46aa83855a6a52600bbd7dea24cc5&seller_online=true`,
+    about: `I am a hardworking game dev who wants to make games that can bring joy to people, to me games are an art form that can make people escape from their realities and let them forget about their stressful daily life, and inspire them by the stories and characters they play. I am skilled in writing, game design, C++ and C#.`,
     // Optional: List your projects, they must have `name` and `description`. `link` is optional.
     projects: [
       {
         name: 'Unity - Lilean',
-        description:
-          'A break neck speed, physics based fps game',
-       link: 'https://findik-bambino.itch.io/lileanproto',
+        description: 'A break neck speed, physics based fps game',
+        link: 'https://findik-bambino.itch.io/lileanproto',
       },
       {
         name: 'Unity - Orhan Quest',
         description:
           'A turn based short experience that features a lot of in jokes',
-        link: 'https://drive.google.com/file/d/1tQLoF-sg4AIpJUeFvvXcZtvskn3MQA9b/view?usp=sharing'
+        link: 'https://drive.google.com/file/d/1tQLoF-sg4AIpJUeFvvXcZtvskn3MQA9b/view?usp=sharing',
       },
       {
         name: 'Unity - Office Rage',
-        description:
-          'Beat-em up game',
-        link: 'https://findik-bambino.itch.io/office-rage'
+        description: 'Beat-em up game',
+        link: 'https://findik-bambino.itch.io/office-rage',
       },
       {
         name: 'Unity - Beyond The Forbidden Lands',
-        description: 'Fan remake of Shadow Of Colossus, that brings cut colossi and areas back to the game',
-        link:'https://www.youtube.com/watch?v=j8OxUJPxAyo&list=PLdHC-rS_Gu4VOzSsczG4ky-Jmj-44BXs4',
+        description:
+          'Fan remake of Shadow Of Colossus, that brings cut colossi and areas back to the game',
+        link: 'https://www.youtube.com/watch?v=j8OxUJPxAyo&list=PLdHC-rS_Gu4VOzSsczG4ky-Jmj-44BXs4',
       },
       {
         name: 'Unity - Climb The Corprate Ladder',
-        description:
-          'A rogue-like game. W.I.P',
-       link: 'https://www.youtube.com/watch?v=JleeCMmT9Jw',
+        description: 'A rogue-like game. W.I.P',
+        link: 'https://www.youtube.com/watch?v=JleeCMmT9Jw',
       },
       {
         name: 'Godot - Climb The Corprate Ladder',
-        description:
-          'RPG game. W.I.P',
-       link: 'https://www.youtube.com/watch?v=HULx85sWroA',
+        description: 'RPG game. W.I.P',
+        link: 'https://www.youtube.com/watch?v=HULx85sWroA',
       },
       {
         name: 'Unity - Canceled Project',
         description:
           'This was a third person action adventure project I was working on with a team, it got canned',
-       link: 'https://github.com/FindikBabmbino/AFAAB',
+        link: 'https://github.com/FindikBabmbino/AFAAB',
       },
       {
         name: 'Python - Quiz Game',
-        description:
-          'Simple quiz game',
-       link: 'https://github.com/FindikBabmbino/Python-QuizGame',
+        description: 'Simple quiz game',
+        link: 'https://github.com/FindikBabmbino/Python-QuizGame',
       },
       {
         name: 'Python - Dice Game',
-        description:
-          'Roll the dice forever!',
-       link: 'https://github.com/FindikBabmbino/Python-DiceGame',
+        description: 'Roll the dice forever!',
+        link: 'https://github.com/FindikBabmbino/Python-DiceGame',
       },
       {
         name: 'Python - Name List',
-        description:
-          'Input names into the list',
-       link: 'https://github.com/FindikBabmbino/Python-NameList',
+        description: 'Input names into the list',
+        link: 'https://github.com/FindikBabmbino/Python-NameList',
       },
       {
         name: 'Python - Rock Paper Scissors',
-        description:
-          '',
-       link: 'https://github.com/FindikBabmbino/Python-RockPaperScissors',
+        description: '',
+        link: 'https://github.com/FindikBabmbino/Python-RockPaperScissors',
       },
       {
         name: 'Python - Astroid Game',
         description:
           'Made with pygame. This project was part of a course I was taking',
-       link: 'https://github.com/FindikBabmbino/Astroid-Game',
+        link: 'https://github.com/FindikBabmbino/Astroid-Game',
       },
       {
         name: 'Python - Frogger Clone',
         description:
           'Made with pygame. This project was part of a course I was taking',
-       link: 'https://github.com/FindikBabmbino/Frogger-Clone',
+        link: 'https://github.com/FindikBabmbino/Frogger-Clone',
       },
       {
         name: 'Python - Western',
         description:
           'Made with pygame. This project was part of a course I was taking',
-       link: 'https://github.com/FindikBabmbino/Western-Game',
+        link: 'https://github.com/FindikBabmbino/Western-Game',
       },
       {
         name: 'Python - Shooter',
         description:
           'Made with pygame. This project was part of a course I was taking',
-       link: 'https://github.com/FindikBabmbino/Shooter-Game',
+        link: 'https://github.com/FindikBabmbino/Shooter-Game',
       },
     ],
     // Optional: List your experience, they must have `name` and `description`. `link` is optional.
@@ -119,7 +112,7 @@ module.exports = {
       {
         name: 'Fiverr',
         description: 'Gameplay Programmer, June 2021-Present',
-          link: 'https://www.fiverr.com/findikbabi/create-new-gameplay-functions-or-fix-your-code?context_referrer=search_gigs&source=top-bar&ref_ctx_id=5b90073188fa00e80b38c1459d7345c1&pckg_id=1&pos=1&context_type=auto&funnel=10b495b9955405d749ca0573bae9141d&seller_online=true',
+        link: 'https://www.fiverr.com/findikbabi/create-new-gameplay-functions-or-fix-your-code?context_referrer=search_gigs&source=top-bar&ref_ctx_id=5b90073188fa00e80b38c1459d7345c1&pckg_id=1&pos=1&context_type=auto&funnel=10b495b9955405d749ca0573bae9141d&seller_online=true',
       },
       {
         name: 'Itch.io',
@@ -128,31 +121,27 @@ module.exports = {
       },
       {
         name: 'Bahcesehir Idea',
-        description: 'This is where I studied game design, it is a part of the Bahcesehir University',
-        link:'',
+        description:
+          'This is where I studied game design, it is a part of the Bahcesehir University',
+        link: '',
       },
       {
         name: 'Baskent University',
-        description: 'This is where I am currently studying translation studies (2020-2024)',
-        link:'',
-      },
-      {
-        name: 'Anadolu University',
-        description: 'This is where I am currently studying computer programming studies (2022-2024)',
-        link:'',
+        description:
+          'This is where I am currently studying translation studies (2020-2024)',
+        link: '',
       },
       {
         name: 'Beyond The Forbidden Lands',
         description: 'I work as a programmer on this project (2022-Present)',
-        link:'http://btflgame.com/',
+        link: 'http://btflgame.com/',
       },
     ],
     // Optional: List your skills, they must have `name` and `description`.
     skills: [
       {
         name: 'Programming Languages',
-        description:
-          'C++, C#, Python',
+        description: 'C++, C#, Python',
       },
       {
         name: 'Game Design',
@@ -161,7 +150,6 @@ module.exports = {
       {
         name: 'Tools',
         description: 'Unity,Unreal,Aseprite,Godot',
-          
       },
     ],
   },
